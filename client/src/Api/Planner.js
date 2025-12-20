d1caf5d9b0acbb18100e53ba1254f220c753f198
sumaiyaakhtar03 <sumaiya22042003@icloud.com>
2025-12-21 02:21:45 +0600

    "API fixed"

--- client/src/Api/Planner.js
+++ client/src/Api/Planner.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3002/api/planner";
+const BASE_URL = "http://localhost:3030/api/planner";
 
 // get tasks for a date
 export const fetchTasksByDate = (date, userId) => {
