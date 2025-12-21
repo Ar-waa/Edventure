b61bb55791b8ddd4d9d2bab700bd3049dcd7bfe2
sumaiyaakhtar03 <sumaiya22042003@icloud.com>
2025-12-21 20:08:20 +0600

    Tags added

--- server/src/controllers/plannerController.js
+++ server/src/controllers/plannerController.js
@@ -15,10 +15,10 @@ const getTasksByDate = async (req, res) => {
 
 // Add a new task
 const addTask = async (req, res) => {
-  const { userId, date, text } = req.body;
+  const { userId, date, text, type } = req.body;
 
   try {
-    const task = new Planner({ userId, date, text });
+    const task = new Planner({ userId, date, text, type });
     await task.save();
     res.status(201).json(task);
   } catch (err) {
@@ -43,5 +43,16 @@ const toggleTask = async (req, res) => {
   }
 };
 
-export { getTasksByDate, addTask, toggleTask };
+const getAllTasks = async (req, res) => {
+  const { userId } = req.query;
+
+  try {
+    const tasks = await Planner.find({ userId });
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching all tasks", error: err });
+  }
+};
+
+export { getTasksByDate, getAllTasks, addTask, toggleTask };
 
--- server/src/models/Planner.js
+++ server/src/models/Planner.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
 const plannerSchema = new mongoose.Schema({
-  userId: { type: String, required: true }, // you can extend this if you add authentication
+  userId: { type: String, required: true },
   date: { type: String, required: true },
   text: { type: String, required: true },
+  type: {
+    type: String,
+    enum: ["exam", "assignment", "quiz", "homework"], 
+  },
   completed: { type: Boolean, default: false },
 });
+
 
 const Planner = mongoose.model("Planner", plannerSchema);
 
